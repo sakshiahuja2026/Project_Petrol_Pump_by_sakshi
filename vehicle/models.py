fb8a289367780a486514d080695fd1c58b543843
sakshiahuja2026@gmail.com <sakshiahuja20262gmail.com>
2021-07-16 21:05:42 +0530

    Data-table modified

--- vehicle/models.py
+++ vehicle/models.py
@@ -6,10 +6,10 @@ from datetime import datetime
 class vehicle(models.Model):
     date=models.DateField(default=datetime.utcnow)
     v_no=models.CharField(max_length=20)
-    driver_name=models.CharField(max_length=25,null=True,blank=True)
+    driver_name=models.CharField(max_length=25, null=True,blank=True)
     choices=[("petrol","petrol"),("diesel","diesel")]
     fuel_type=models.CharField(max_length=10,choices=choices)
-    remarks=models.CharField(max_length=40)
+    remarks=models.CharField(max_length=50, null=True,blank=True)
     creditor=models.ForeignKey(creditor_master,on_delete=models.CASCADE,related_name='vehicle')
 
     def __str__(self):
